fe07c1f29afd812fa4bea7f36ac185508f285a07
Jaeger <jaegerCode@gmail.com>
2025-03-21 18:50:07 +0800

    feat: ignore https error

--- src/tools/fetchUrls.ts
+++ src/tools/fetchUrls.ts
@@ -104,6 +104,7 @@ export async function fetchUrls(args: any) {
     browser = await chromium.launch({ headless: !useDebugMode });
     const context = await browser.newContext({
       javaScriptEnabled: true,
+      ignoreHTTPSErrors: true,
       userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     });
 
