9707a218cc96beb75ba039eaa36d518415895590
naru-hiyoko <atsushi.narusawa@access-company.com>
2026-04-26 23:43:08 +0900

    fetch_urlツールを削除して機能を統一

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,11 +1,9 @@
-import { fetchUrlTool, fetchUrl } from './fetchUrl.js';
 import { fetchUrlsTool, fetchUrls } from './fetchUrls.js';
 import { closeBrowserTool, closeBrowser } from './closeBrowser.js';
 import { openBrowserForLoginTool, openBrowserForLogin } from './openBrowserForLogin.js';
 
 // Export tool definitions
 export const tools = [
-  fetchUrlTool,
   fetchUrlsTool,
   closeBrowserTool,
   openBrowserForLoginTool
@@ -13,7 +11,6 @@ export const tools = [
 
 // Export tool implementations
 export const toolHandlers = {
-  [fetchUrlTool.name]: fetchUrl,
   [fetchUrlsTool.name]: fetchUrls,
   [closeBrowserTool.name]: closeBrowser,
   [openBrowserForLoginTool.name]: openBrowserForLogin
